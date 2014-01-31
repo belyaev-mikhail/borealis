7f1c9adf539f7fb50e3b115c053faf9b6e7334a3
Sam Kolton <samkolton@gmail.com>
2014-01-31 18:11:29 +0400

    Added TestGenerationPass and getTest method to Z3::Solver

---
+++ Passes/TestGeneration/TestGenerationPass.cpp
@@ -0,0 +1,94 @@
+/*
+ * TestGenerationPass.cpp
+ *
+ *  Created on: Jan 27, 2014
+ *      Author: sam
+ */
+
+
+#include "Codegen/intrinsics_manager.h"
+#include "Passes/TestGeneration/TestGenerationPass.h"
+#include "Passes/Tracker/SlotTrackerPass.h"
+#include "SMT/MathSAT/Solver.h"
+#include "SMT/Z3/Solver.h"
+#include "Util/util.h"
+
+namespace borealis {
+
+TestGenerationPass::TestGenerationPass() : ProxyFunctionPass(ID) {}
+TestGenerationPass::TestGenerationPass(llvm::Pass* pass) : ProxyFunctionPass(ID, pass) {}
+
+void TestGenerationPass::getAnalysisUsage(llvm::AnalysisUsage& AU) const {
+    AU.setPreservesAll();
+
+    AUX<FunctionManager>::addRequiredTransitive(AU);
+    AUX<PredicateStateAnalysis>::addRequiredTransitive(AU);
+    AUX<SlotTrackerPass>::addRequiredTransitive(AU);
+}
+
+bool TestGenerationPass::shouldSkipFunction(llvm::Function* F) {
+    // skip borealis_globals function
+    auto& im = IntrinsicsManager::getInstance();
+    if (im.getIntrinsicType(F) == function_type::INTRINSIC_GLOBAL_DESCRIPTOR_TABLE)
+        return true;
+
+    if ( !llvm::getSingleRetOpt(F)) // no return in function
+        return true;
+
+    // XXX sam How to determine main function?
+    if (F->getName() == "__main" || F->getName() == "main") // skip main function
+        return true;
+
+    return false;
+}
+
+bool TestGenerationPass::runOnFunction(llvm::Function& F) {
+
+    if (shouldSkipFunction(&F))
+        return false;
+
+    FM = &GetAnalysis<FunctionManager>::doit(this, F);
+    PSA = &GetAnalysis<PredicateStateAnalysis>::doit(this, F);
+
+    auto* st = GetAnalysis<SlotTrackerPass>::doit(this, F).getSlotTracker(F);
+    FN = FactoryNest(st);
+
+    dbgs() << "name: " << F.getName() << endl
+           << "req: "  << FM->getReq(&F) << endl
+           << "ens: "  << FM->getEns(&F) << endl;
+
+    auto fMemId = FM->getMemoryStart(&F);
+
+#if defined USE_MATHSAT_SOLVER
+    MathSAT::ExprFactory ef;
+    MathSAT::Solver s(ef, fMemId);
+#else
+    Z3::ExprFactory ef;
+    Z3::Solver s(ef, fMemId);
+#endif
+
+    std::vector<Term::Ptr> args;
+    args.reserve(F.arg_size());
+    for (auto& arg : util::view(F.arg_begin(), F.arg_end())) {
+        args.push_back(FN.Term->getArgumentTerm(&arg));
+    }
+
+    auto* ret = llvm::getSingleRetOpt(&F);
+
+    std::string str = "test case: \n";
+    for (const auto& testValue : s.getTest(PSA->getInstructionState(ret), args)) {
+        str += testValue.first->getName() + " = " +
+               util::toString(testValue.second) + "\n";
+    }
+    dbgs() << str;
+
+    return false;
+}
+
+TestGenerationPass::~TestGenerationPass() {}
+
+char TestGenerationPass::ID;
+static RegisterPass<TestGenerationPass>
+X("test-generation", "Pass that generates unit tests for.");
+
+} /* namespace borealis */
--- SMT/Z3/Solver.h
+++ SMT/Z3/Solver.h
@@ -8,6 +8,8 @@
 #ifndef Z3_SOLVER_H_
 #define Z3_SOLVER_H_
 
+#include <unordered_map>
+
 #include "Logging/logger.hpp"
 #include "SMT/Z3/ExecutionContext.h"
 #include "SMT/Z3/ExprFactory.h"
@@ -24,6 +26,9 @@ class Solver : public borealis::logging::ClassLevelLogging<Solver> {
 
 public:
 
+    typedef std::unordered_map<Term::Ptr, Dynamic> Test;
+//    typedef std::unordered_map<Term::Ptr, Dynamic, util::hash::defaultHasher> Test;
+
 #include "Util/macros.h"
     static constexpr auto loggerDomain() QUICK_RETURN("z3-solver")
 #include "Util/unmacros.h"
@@ -37,6 +42,10 @@ public:
     bool isPathImpossible(
             PredicateState::Ptr path,
             PredicateState::Ptr state);
+
+    Test getTest(
+            PredicateState::Ptr state,
+            std::vector<Term::Ptr>& args);
 
 private:
 
