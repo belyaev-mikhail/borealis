cfd28b3372d322875b787494c860729133e53578
Maxim Petrov <maximpetrov@yahoo.com>
2014-03-26 15:52:15 +0400

    output filename now contains original file name

--- Actions/FindPrototypesVisitor.cpp
+++ Actions/FindPrototypesVisitor.cpp
@@ -5,6 +5,8 @@
  * Created on 25 Март 2014 г., 12:18
  */
 
+#include <llvm/Support/PathV2.h>
+
 #include "Actions/FindPrototypesVisitor.h"
 #include "TestGen/PrototypesInfo.h"
 
@@ -12,8 +14,8 @@ namespace borealis {
 
 bool FindPrototypesVisitor::VisitFunctionDecl(clang::FunctionDecl* s) {
     if (s->isFirstDeclaration()) {
-        std::string fileName = sm->getFileEntryForID(sm->getFileID(s->getLocation()))->getName();
-        if (fileName.back() == 'h') {
+        llvm::StringRef fileName = sm->getFileEntryForID(sm->getFileID(s->getLocation()))->getName();
+        if (llvm::sys::path::extension(fileName) == ".h") {
             prototypes->locations.emplace(s->getNameAsString(), fileName);
         }
     }
