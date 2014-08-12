798938b3f7195f1969fa0f98986b625cf9a0aa0e
Maxim Petrov <maximpetrov@yahoo.com>
2014-08-12 11:36:24 +0400

    some fixes in output

--- Passes/TestGeneration/TestStatisticsDumpPass.cpp
+++ Passes/TestGeneration/TestStatisticsDumpPass.cpp
@@ -34,28 +34,29 @@ bool TestStatisticsDumpPass::runOnModule(llvm::Module & M) {
     
     std::cout << "Test generation statistics:" << std::endl;
     
-    std::cout << stat->getTotalGood() << " tests generated";
+    std::cout << "\t" << stat->getTotalGood() << " tests generated" << std::endl;
     auto totalStubs = stat->getTotalStubs();
     if (totalStubs > 0) {
-        std::cout << " (" << totalStubs << " with stubs)";
+        std::cout << "\t" << totalStubs << " stubs generated" << std::endl;
     }
+    
     std::cout << std::endl;
     
-    auto totalUnknown = stat->getTotalUnknown();
+    /*auto totalUnknown = stat->getTotalUnknown();
     if (totalUnknown > 0) {
         std::cout << totalUnknown << " tests failed to generate" << std::endl;
-    }
+    }*/
     
     std::cout << "Statistics by functions:" << std::endl;
     
     for (auto fs: *stat) {
-        std::cout << fs.first->getName() << ": " << fs.second.good << " tests";
+        std::cout << "\t" << fs.first->getName() << ": " << fs.second.good << " tests";
         if (fs.second.stubs > 0) {
             std::cout << " with stubs";
         }
-        if (fs.second.unknown > 0) {
+        /*if (fs.second.unknown > 0) {
             std::cout << ", " << fs.second.unknown << " tests failed to generate";
-        }
+        }*/
         std::cout << std::endl;
     }
     
