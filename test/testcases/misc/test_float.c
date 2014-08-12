d5235519466b76708121b76c9dab392cb2a9d5e0
Sam Kolton <samkolton@gmail.com>
2014-08-12 12:25:34 +0400

    sine

--- test/testcases/misc/test_float.c
+++ test/testcases/misc/test_float.c
@@ -12,36 +12,15 @@ float foo(double a, float b) {
     return c;
 }
 
-int bar(double d, unsigned i) {
-    if (d > 2.8)
-        return i > d;
-    else 
-        return 0;
-}
-
-double cosinus(double x) {
-    double t , s ;
-    int p;
-    p = 0;
-    s = 1.0;
-    t = 1.0;
-    while(fabs(t/s) > 0.001) {
-        p++;
-        t = (-t * x * x) / ((2 * p - 1) * (2 * p));
-        s += t;
-    }
-    return s;
-}
-
 // @ensures (\result <= 1) && (\result >= -1)
 float sine(float x) {
     float res = 0, pow = x, fact = 1;
     int ii;
-    // if (x == M_PI) {
-    //     return 0;
-    // } else if (x == M_PI / 2) {
-    //     return 1;
-    // }
+    if (x == M_PI) {
+        return 0.0;
+    } else if (x == M_PI / 2) {
+        return 1.0;
+    }
     for(ii = 0; ii < 100; ++ii) {
         float i = (float) ii;
         res+=pow / fact;
@@ -50,13 +29,3 @@ float sine(float x) {
     }
     return res;
 }
-
-
-float smth(float a) {
-    for (int i = 0; i < 100; ++i) {
-        a = a + 2.0;
-        float ii = (float)i;
-    }
-    return a;
-}
-
