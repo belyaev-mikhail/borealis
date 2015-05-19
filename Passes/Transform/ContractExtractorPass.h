f5986023ff8b52e92d13fccb385fa74b800124c3
azat.aam <azat.aam@localhost>
2015-05-19 15:51:21 +0300

    added Contract extractor pass

---
+++ Passes/Transform/ContractExtractorPass.h
@@ -0,0 +1,27 @@
+/*
+ * ContractExtractor.h
+ *
+ *  Created on: 19 мая 2015 г.
+ *      Author: kivi
+ */
+
+#ifndef PASSES_TRANSFORM_CONTRACTEXTRACTORPASS_H_
+#define PASSES_TRANSFORM_CONTRACTEXTRACTORPASS_H_
+
+#include "llvm/Pass.h"
+
+namespace borealis {
+
+class ContractExtractorPass : public llvm::FunctionPass {
+public:
+    static char ID;
+
+    ContractExtractorPass();
+    virtual ~ContractExtractorPass() = default;
+
+    virtual bool runOnFunction(llvm::Function& F) override;
+};
+
+} /* namespace borealis */
+
+#endif /* PASSES_TRANSFORM_CONTRACTEXTRACTORPASS_H_ */
