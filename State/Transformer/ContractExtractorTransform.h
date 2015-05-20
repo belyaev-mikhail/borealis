b390f8d7206945deb4d53d2d95ae116570e95d5d
Azat Abdullin <azat.aam@gmail.com>
2015-05-20 18:56:23 +0300

    added transform into ContractExtractionPass

--- State/Transformer/ContractExtractorTransform.h
+++ State/Transformer/ContractExtractorTransform.h
@@ -8,28 +8,41 @@
 #ifndef STATE_TRANSFORMER_CONTRACTEXTRACTORTRANSFORM_H_
 #define STATE_TRANSFORMER_CONTRACTEXTRACTORTRANSFORM_H_
 
+#include "Factory/Nest.h"
 #include "State/Transformer/Transformer.hpp"
 #include "llvm/IR/Function.h"
+#include <vector>
 
 namespace borealis {
 
 class ContractExtractorTransform: public borealis::Transformer<ContractExtractorTransform> {
 
-	typedef borealis::Transformer<ContractExtractorTransform> Base;
+	using Base = borealis::Transformer<ContractExtractorTransform>;
+	using Args = std::vector<Term::Ptr>;
 
 public:
 
-	ContractExtractorTransform(llvm::Function* f) :
+	ContractExtractorTransform(FactoryNest fn, llvm::Function* f) :
 		Base(FactoryNest()),
-		func(f) {}
+		FN(fn) {
+		for(auto&& it : f->args()) {
+			args.push_back(FN.Term->getArgumentTerm(&it));
+		}
+	}
 
 	Predicate::Ptr trasformBase(Predicate::Ptr p) {
 		return p;
 	}
 
+	Predicate::Ptr transformPredicate(Predicate::Ptr p) {
+		return p;
+	}
+
 private:
 
-	llvm::Function* func;
+	Args args;
+	FactoryNest FN;
+
 
 };
 
