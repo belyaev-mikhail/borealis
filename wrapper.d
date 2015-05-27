ac494261d6d60f0514383e3650193818d40f5951
azat.aam <azat.aam@localhost>
2015-05-27 13:15:05 +0300

    remove unused files

--- wrapper.d
+++
@@ -1,8 +0,0 @@
-/home/danya/borealis/wrapper.o /home/danya/borealis/wrapper.d: /home/danya/borealis/wrapper.cpp \
-  /home/danya/borealis/Driver/gestalt.h \
-  /home/danya/borealis/Logging/logger.hpp \
-  /home/danya/borealis/lib/tinyformat/tinyformat.h \
-  /home/danya/borealis/Logging/log_entry.hpp \
-  /home/danya/borealis/Logging/logstream.hpp \
-  /home/danya/borealis/Util/macros.h \
-  /home/danya/borealis/Util/unmacros.h
