52d243364b23f8a47000cab2d5ad9833103ecb70
stepanov <14219946+DaniilStepanov@users.noreply.github.com>
2015-10-15 16:50:31 +0300

    bigfixes FuncSum

--- State/Transformer/EqualityMapper.cpp
+++ State/Transformer/EqualityMapper.cpp
@@ -9,48 +9,52 @@
 
 namespace borealis {
 
-EqualityMapper::EqualityMapper(FactoryNest FN) : Base(FN) {}
+    EqualityMapper::EqualityMapper(FactoryNest FN) : Base(FN) {}
 
-Predicate::Ptr EqualityMapper::transformEqualityPredicate(EqualityPredicatePtr pred) {
-    if (util::at(mapping, pred->getLhv())) return pred;
+    Predicate::Ptr EqualityMapper::transformEqualityPredicate(EqualityPredicatePtr pred) {
+        if (util::at(mapping, pred->getLhv()) || llvm::isa<ReturnValueTerm>(pred->getLhv())) return pred;
 
-    TermMap replacement;
-    for (auto&& subterm : Term::getFullTermSet(pred->getRhv())) {
-        if (auto&& value = util::at(mapping, subterm)) {
-            replacement[subterm] = value.getUnsafe();
+        TermMap replacement;
+        for (auto&& subterm : Term::getFullTermSet(pred->getRhv())) {
+            if (auto&& value = util::at(mapping, subterm)) {
+                replacement[subterm] = value.getUnsafe();
+            }
         }
-    }
-    auto&& newRhv = Term::Ptr{ pred->getRhv()->replaceOperands(replacement) };
-    auto&& replaced = FN.Predicate->getEqualityPredicate(pred->getLhv(), newRhv, pred->getLocation(), pred->getType());
-    replaced = Predicate::Ptr{ replaced->replaceOperands(replacement) };
+        auto&& newRhv = Term::Ptr{ pred->getRhv()->replaceOperands(replacement) };
+        auto&& replaced = FN.Predicate->getEqualityPredicate(pred->getLhv(), newRhv, pred->getLocation(), pred->getType());
+        replaced = Predicate::Ptr{ replaced->replaceOperands(replacement) };
 
-    if (auto&& newEq = llvm::dyn_cast<EqualityPredicate>(replaced)) {
-        if (not isOpaqueTerm(newEq->getRhv())) {
-            mapping[newEq->getLhv()] = newEq->getRhv();
+        if (auto&& newEq = llvm::dyn_cast<EqualityPredicate>(replaced)) {
+            if (not isOpaqueTerm(newEq->getRhv())) {
+                if (auto&& castTerm = llvm::dyn_cast<CastTerm>(newEq->getRhv())) {
+                    mapping[newEq->getLhv()] = castTerm->getRhv();
+                } else {
+                    mapping[newEq->getLhv()] = newEq->getRhv();
+                }
+            }
         }
+        return replaced;
     }
-    return replaced;
-}
 
-Predicate::Ptr EqualityMapper::transformPredicate(Predicate::Ptr pred) {
-    if (pred->getType() == PredicateType::PATH) {
-        return Predicate::Ptr{ pred->replaceOperands(mapping) };
+    Predicate::Ptr EqualityMapper::transformPredicate(Predicate::Ptr pred) {
+        if (pred->getType() == PredicateType::PATH) {
+            return Predicate::Ptr{ pred->replaceOperands(mapping) };
+        }
+        return pred;
     }
-    return pred;
-}
 
-const EqualityMapper::TermMap& EqualityMapper::getMappedValues() const {
-    return mapping;
-}
+    const EqualityMapper::TermMap& EqualityMapper::getMappedValues() const {
+        return mapping;
+    }
 
-bool EqualityMapper::isOpaqueTerm(Term::Ptr term) {
-    return llvm::is_one_of<
-            OpaqueBoolConstantTerm,
-            OpaqueIntConstantTerm,
-            OpaqueFloatingConstantTerm,
-            OpaqueStringConstantTerm,
-            OpaqueNullPtrTerm
-    >(term);
-}
+    bool EqualityMapper::isOpaqueTerm(Term::Ptr term) {
+        return llvm::is_one_of<
+                OpaqueBoolConstantTerm,
+                OpaqueIntConstantTerm,
+                OpaqueFloatingConstantTerm,
+                OpaqueStringConstantTerm,
+                OpaqueNullPtrTerm
+        >(term);
+    }
 
 }  /* namespace borealis */
