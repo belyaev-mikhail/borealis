9e41bb19577fd50ed71662dff0f71058aba5ce20
Azat Abdullin <azat.aam@gmail.com>
2015-10-22 16:53:11 +0300

    added simple BasicStates merging in ContractManager

--- Passes/Manager/ContractManager.cpp
+++ Passes/Manager/ContractManager.cpp
@@ -7,6 +7,8 @@
 
 #include <State/Transformer/Unifier.h>
 #include <State/Transformer/StateOptimizer.h>
+#include <State/Transformer/MergingTransformer.h>
+
 #include "ContractManager.h"
 #include "State/Transformer/StateChoiceKiller.h"
 
@@ -71,51 +73,8 @@ void ContractManager::addSummary(llvm::Function* F, const FactoryNest& FN, Predi
 }
 
 void ContractManager::print(llvm::raw_ostream&, const llvm::Module*) const {
-    auto&& dbg = dbgs();
-
-    dbg << "Contract extraction results" << endl;
-    for (auto&& it : functionCalls) {
-        if (not util::containsKey(basicContracts, it.first) && not util::containsKey(choiceContracts, it.first)) {
-            continue;
-        }
-        dbg << "---" << "Function " << it.first->getName() << "---" << endl;
-        dbg << "Called " << it.second << " times" << endl;
-        dbg << endl;
-
-        for (auto&& state : basicContracts[it.first]) {
-            dbg << "State:" << endl;
-            dbg << state << endl;
-        }
-
-        if (it.second > 1) {
-            std::vector<std::pair<PredicateState::Ptr, int>> choices;
-            getUniqueChoices(it.first, choices);
-            for (auto&& it_choice : choices) {
-                if (double(it_choice.second)/double(it.second) > 0.66) {
-                    dbg << "State:" << endl;
-                    dbg << it_choice.first << endl;
-                }
-            }
-        }
-
-        dbg << endl;
-    }
-    dbg << end;
-
-    dbg << "Summary extraction results" << endl;
-    for (auto&& it : summaries) {
-        dbg << "---" << "Function " << it.first->getName() << "---" << endl;
-        dbg << "Called " << functionCalls[it.first] << " times" << endl;
-        dbg << endl;
-
-        for (auto&& state : it.second) {
-            dbg << "State:" << endl;
-            dbg << state << endl;
-        }
-
-        dbg << endl;
-    }
-    dbg << end;
+    printContracts();
+    printSummaries();
 }
 
 void ContractManager::saveState(llvm::Function* F, PredicateState::Ptr S) {
@@ -144,6 +103,69 @@ void ContractManager::getUniqueChoices(llvm::Function* F, std::vector<std::pair<
             res.push_back({state, 1});
         }
     }
+}
+
+void ContractManager::printContracts() const {
+    ContractStates result;
+
+    //analyzing results
+    for (auto&& it : functionCalls) {
+        auto&& F = it.first;
+        auto&& calls = it.second;
+        //merging basic states for each function, probability 0.66
+        auto&& merger = MergingTransformer(FactoryNest(nullptr), calls);
+        for (auto&& state : basicContracts[F]) {
+            merger.transform(state);
+        }
+        auto&& merged = merger.getMergedState(0.66);
+        if (not merged->isEmpty()) {
+            result[F].insert(merged);
+        }
+        //merging choice states for each function, probability 0.66
+        if (calls > 1) {
+            std::vector<std::pair<PredicateState::Ptr, int>> choices;
+            getUniqueChoices(F, choices);
+            for (auto&& it_choice : choices) {
+                if (double(it_choice.second)/double(calls) > 0.66) {
+                    result[F].insert(it_choice.first);
+                }
+            }
+        }
+    }
+
+    //printing results
+    auto&& dbg = dbgs();
+
+    dbg << "Contract extraction results" << endl;
+    for (auto&& it : result) {
+        auto&& F = it.first;
+        dbg << "---" << "Function " << F->getName() << "---" << endl;
+
+        for(auto&& state : it.second) {
+            dbg << "State:" << endl;
+            dbg << state << endl;
+        }
+
+        dbg << endl;
+    }
+    dbg << end;
+}
+
+void ContractManager::printSummaries() const {
+    auto&& dbg = dbgs();
+
+    dbg << "Summary extraction results" << endl;
+    for (auto&& it : summaries) {
+        dbg << "---" << "Function " << it.first->getName() << "---" << endl;
+
+        for (auto&& state : it.second) {
+            dbg << "State:" << endl;
+            dbg << state << endl;
+        }
+
+        dbg << endl;
+    }
+    dbg << end;
 }
 
 char ContractManager::ID = 0;
