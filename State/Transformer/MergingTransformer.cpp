39ee1b0ce4ad4205625669ef8155da11bbb7af9b
Azat Abdullin <azat.aam@gmail.com>
2015-12-10 19:12:01 +0300

    added Solver::isPossible(Predicate::Ptr, Predicate::Ptr), changed MargingTransformer

--- State/Transformer/MergingTransformer.cpp
+++ State/Transformer/MergingTransformer.cpp
@@ -10,15 +10,11 @@
 
 namespace borealis {
 
-MergingTransformer::MergingTransformer(const FactoryNest& fn, MemInfo& memInfo, TermSet args, int calls)
-        : Base(fn), FN(fn), fMemInfo(memInfo), arguments(args), functionCalls(calls) {}
+MergingTransformer::MergingTransformer(const FactoryNest& fn, MemInfo& memInfo, int calls)
+        : Base(fn), FN(fn), fMemInfo(memInfo), functionCalls(calls) {}
 
 Predicate::Ptr MergingTransformer::transformPredicate(Predicate::Ptr pred) {
-    for (auto&& arg : arguments) {
-        if (containsTerm(pred, arg)) {
-            ++contractsByArg[arg][pred];
-        }
-    }
+    ++contracts[pred];
     return pred;
 }
 
@@ -27,40 +23,35 @@ PredicateState::Ptr MergingTransformer::getMergedState() {
     Z3::Solver s(ef, fMemInfo.first, fMemInfo.second);
 
     std::vector<Predicate::Ptr> result;
+    PredicateSet forDelete;
 
-    for (auto&& it : contractsByArg) {
-        std::vector<Predicate::Ptr> currMerge;
-        PredicateSet forDelete;
-        for (auto&& predCounter : it.second) {
-            if (currMerge.empty()) currMerge.push_back(predCounter.first);
-            else {
-                for (auto&& pred : currMerge) {
-                    if (pred == nullptr) continue;
-                    auto&& res = s.getStrongerPredicate(predCounter.first, pred);
-                    if (res == nullptr) forDelete.insert(pred);
-                    else if (res->equals(predCounter.first.get())) {
-                        forDelete.insert(pred);
-                        currMerge.push_back(predCounter.first);
-                    }
+    for (auto&& it : contracts) {
+        result.push_back(it.first);
+    }
+
+    for (auto i = 0U; i < result.size(); ++i) {
+        for (auto j = i + 1; j < result.size(); ++j) {
+            if (s.isPossible(result[i], result[j]).isSat()) {
+                if (s.isStronger(result[i], result[j]).isUnsat()) {
+                    forDelete.insert(result[j]);
+                } else if (s.isStronger(result[j],result[i]).isUnsat()) {
+                    forDelete.insert(result[i]);
                 }
+            } else {
+                forDelete.insert(result[i]);
+                forDelete.insert(result[j]);
             }
         }
-        for (auto&& pred : currMerge) {
-            if (not util::contains(forDelete, pred))
-                result.push_back(pred);
+    }
+
+    for (auto i = 0U; i < result.size(); ++i) {
+        if (util::contains(forDelete, result[i])) {
+            result.erase(result.begin() + i);
+            --i;
         }
     }
 
     return FN.State->Basic(result);
-}
-
-bool MergingTransformer::containsTerm(Predicate::Ptr predicate, Term::Ptr term) {
-    for (auto&& op : predicate->getOperands()) {
-        if (util::contains(Term::getFullTermSet(op), term)) {
-            return true;
-        }
-    }
-    return false;
 }
 
 }   /* namespace borealis */
