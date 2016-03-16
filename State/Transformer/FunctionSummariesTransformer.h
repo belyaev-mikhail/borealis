51f89ca431933a1da3349b66640b01601a1cc471
stepanov <stepanov@localhost>
2016-03-16 18:43:40 +0300

    Simplify SummariesTransformer

--- State/Transformer/FunctionSummariesTransformer.h
+++ State/Transformer/FunctionSummariesTransformer.h
@@ -11,6 +11,9 @@
 #include <unordered_set>
 
 #include "State/Transformer/Transformer.hpp"
+#include "State/Transformer/StateSlicer.h"
+
+#include "Util/algorithm.hpp"
 
 namespace borealis {
 
@@ -19,16 +22,13 @@ class FunctionSummariesTransformer : public borealis::Transformer<FunctionSummar
     using Base = borealis::Transformer<FunctionSummariesTransformer>;
 
     using TermSet = std::unordered_set<Term::Ptr,TermHash, TermEquals>;
-    using TermMap = std::unordered_map<Term::Ptr, Term::Ptr, TermHash, TermEquals>;
     using PredTermMap = std::unordered_map<Predicate::Ptr, Term::Ptr, PredicateHash, PredicateEquals>;
-    using ChoiceInfo = std::vector<std::vector<Predicate::Ptr>>;
     using PrVector = std::vector<Predicate::Ptr>;
     using VecTermSet = std::vector<std::unordered_set<Term::Ptr,TermHash, TermEquals>>;
 
 
 public:
-    FunctionSummariesTransformer(const FactoryNest& FN, const TermMap& TM,
-                const ChoiceInfo& ci, const Term::Ptr rvt);
+    FunctionSummariesTransformer(const FactoryNest& FN, const Term::Ptr rtvMap);
 
     using Base::transform;
     PredicateState::Ptr transform(PredicateState::Ptr ps);
@@ -47,21 +47,18 @@ public:
     }
 
 private:
-    bool isDepFromArg(Term::Ptr term);
     bool isOpaqueTerm(Term::Ptr term);
 
 private:
 
-    TermMap mapping;
     TermSet TS;
     VecTermSet ter;
     PrVector protPreds;
-
-    ChoiceInfo choiceInfo;
-    Term::Ptr rtv;
-    int curPredi;
     PredTermMap protPredMapping;
+    Predicate::Ptr curPathPr;
+    Term::Ptr rtvMapping;
 };
 
 } /*namespace borealis*/
 #endif /* STATE_TRANSFORMER_FUNCTIONSUMMARIESTRANSFORMER_H_ */
+
