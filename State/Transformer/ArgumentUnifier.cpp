0758f6c7bd470ff6ff3dab4056367ffed2231660
azat.aam <azat.aam@localhost>
2016-03-25 14:07:39 +0300

    ignoring contracts, that use local varibles

--- State/Transformer/ArgumentUnifier.cpp
+++ State/Transformer/ArgumentUnifier.cpp
@@ -10,6 +10,21 @@ ArgumentUnifier::ArgumentUnifier(const FactoryNest& fn, const ArgToTerms& a)
         : Base(fn), FN(fn), argsMapping(a) {
 }
 
+PredicateState::Ptr ArgumentUnifier::transform(PredicateState::Ptr ps) {
+    return Base::transform(ps)
+            ->filter([](auto&& p) { return !!p; })
+            ->simplify();
+}
+
+Predicate::Ptr ArgumentUnifier::transformPredicate(Predicate::Ptr pred) {
+    for (auto&& op : pred->getOperands()) {
+        if (not allowedInContract(op)) {
+            return nullptr;
+        }
+    }
+    return pred;
+}
+
 Term::Ptr ArgumentUnifier::transformTerm(Term::Ptr term) {
     for (auto&& it : argsMapping) {
         if (util::contains(it.second, term)) {
@@ -22,13 +37,29 @@ Term::Ptr ArgumentUnifier::transformTerm(Term::Ptr term) {
     return term;
 }
 
-bool ArgumentUnifier::isSigned(llvm::ConditionType cond) {
-    using CondType = llvm::ConditionType;
-    return cond != CondType::ULT && cond != CondType::ULE && cond != CondType::UGE && cond != CondType::UGT;
-}
-
 const ArgumentUnifier::TermSet& ArgumentUnifier::getArguments() {
     return args;
+}
+
+bool ArgumentUnifier::allowedInContract(Term::Ptr term) const {
+    for (auto&& t : term->getSubterms()) {
+        auto&& valTerm = llvm::dyn_cast<ValueTerm>(t);
+        auto isGlobal = valTerm && valTerm->isGlobal();
+        if (not isOpaqueTerm(t) && not llvm::isa<ArgumentTerm>(t) && not isGlobal) {
+            return false;
+        }
+    }
+    return true;
+}
+
+bool ArgumentUnifier::isOpaqueTerm(Term::Ptr term) const {
+    return llvm::is_one_of<
+            OpaqueBoolConstantTerm,
+            OpaqueIntConstantTerm,
+            OpaqueFloatingConstantTerm,
+            OpaqueStringConstantTerm,
+            OpaqueNullPtrTerm
+    >(term);
 }
 
 }   /* namespace borealis */
