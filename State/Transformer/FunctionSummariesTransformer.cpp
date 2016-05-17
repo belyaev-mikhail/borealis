085c8668c642c9f3a8f058c9af483562d00d4949
stepanov <stepanov@localhost>
2016-05-17 15:36:49 +0300

    Merge

--- State/Transformer/FunctionSummariesTransformer.cpp
+++ State/Transformer/FunctionSummariesTransformer.cpp
@@ -6,12 +6,12 @@
  */
 
 #include "FunctionSummariesTransformer.h"
-
+#include <vector>
 
 namespace borealis{
 
-FunctionSummariesTransformer::FunctionSummariesTransformer(const FactoryNest& FN, const Term::Ptr rtvMap) :
-        Base(FN),rtvMapping(rtvMap) { }
+FunctionSummariesTransformer::FunctionSummariesTransformer(const FactoryNest& FN, const TermSet rtvMap) :
+        Base(FN), rtvEquiv(rtvMap) {}
 
 PredicateState::Ptr FunctionSummariesTransformer::transform(PredicateState::Ptr ps) {
     return Base::transform(ps)
@@ -21,30 +21,45 @@ PredicateState::Ptr FunctionSummariesTransformer::transform(PredicateState::Ptr 
 }
 
 
+PredicateState::Ptr FunctionSummariesTransformer::transformChoice(PredicateStateChoicePtr ps){
+    Predicate::Ptr emptyPred;
+    prStack.push(emptyPred);
+    return Base::transformChoice(ps);
+}
+
+
+PredicateState::Ptr FunctionSummariesTransformer::transformPredicateStateChoice(PredicateStateChoicePtr ps){
+    prStack.pop();
+    return ps;
+}
+
+
 Predicate::Ptr FunctionSummariesTransformer::transformPredicate(Predicate::Ptr pred) {
     if(pred->getType()==PredicateType::PATH){
-        curPathPr=pred;
+        if(prStack.size()!=0){
+            prStack.pop();
+        }
+        prStack.push(pred);
         return pred;
     }
     else{
         auto&& eqPred=llvm::dyn_cast<EqualityPredicate>(pred);
         if(eqPred==NULL) return pred;
-        if(not(eqPred->getLhv()->equals(rtvMapping.get()))) return pred;
+        if(rtvEquiv.find(eqPred->getLhv())==rtvEquiv.end()) return pred;
         if(not isOpaqueTerm(eqPred->getRhv())) return pred;
-        for (auto &&op : curPathPr->getOperands()) {
+        for (auto &&op : prStack.top()->getOperands()) {
             for (auto&& t : Term::getFullTermSet(op)) {
                 if(!isOpaqueTerm(t)&&t->getNumSubterms()==0){
                     TS.insert(t);
-                    if(!util::contains(protPreds,curPathPr))
-                        protPreds.push_back(curPathPr);
-                    protPredMapping.insert(std::make_pair(curPathPr,eqPred->getRhv()));
+                    if(!util::contains(protPreds,prStack.top()))
+                        protPreds.push_back(prStack.top());
+                    protPredMapping.insert(std::make_pair(prStack.top(),eqPred->getRhv()));
                 }
             }
             if(TS.size()!=0)
                 ter.push_back(TS);
             TS.clear();
         }
-
     }
     return pred;
 }
