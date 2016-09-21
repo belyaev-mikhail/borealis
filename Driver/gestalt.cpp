b7c4c6b286d53da7c59917c30632e95b5b190945
Azat Abdullin <azat.aam@gmail.com>
2016-09-21 14:04:16 +0300

    added llvm_function_pipeline (P.S. its not working

--- Driver/gestalt.cpp
+++ Driver/gestalt.cpp
@@ -69,12 +69,13 @@
 #include "Driver/clang_pipeline.h"
 #include "Driver/gestalt.h"
 #include "Driver/interviewer.h"
-#include "Driver/llvm_pipeline.h"
+#include "Driver/llvm_module_pipeline.h"
 #include "Driver/plugin_loader.h"
 #include "Logging/logger.hpp"
 #include "Passes/Misc/PrinterPasses.h"
 #include "Passes/Util/DataProvider.hpp"
 #include "Util/util.h"
+#include "llvm_function_pipeline.h"
 
 namespace borealis {
 namespace driver {
@@ -175,7 +176,6 @@ int gestalt::main(int argc, const char** argv) {
     std::vector<StringRef> passes2run;
     passes2run.insert(passes2run.end(), prePasses.begin(), prePasses.end());
     passes2run.insert(passes2run.end(), inPasses.begin(), inPasses.end());
-    passes2run.insert(passes2run.end(), postPasses.begin(), postPasses.end());
 
     std::vector<StringRef> libs2load;
     libs2load.insert(libs2load.end(), libs.begin(), libs.end());
@@ -209,7 +209,7 @@ int gestalt::main(int argc, const char** argv) {
 
     // run llvm passes
 
-    llvm_pipeline llvm { module_ptr };
+    llvm_module_pipeline llvm { module_ptr };
     llvm.assignLogger(*this);
 
     llvm.add(*annotatedModule->annotations);
@@ -220,7 +220,41 @@ int gestalt::main(int argc, const char** argv) {
         llvm.add(pass.str());
     }
 
+    llvm::errs() << "FUCK" << *module_ptr << '\n';
+
     llvm.run();
+
+    llvm::errs() << "SHIT" << *module_ptr << '\n';
+
+    passes2run.clear();
+    passes2run.insert(passes2run.end(), postPasses.begin(), postPasses.end());
+
+    {
+        borealis::logging::log_entry out(infos());
+        out << "Function passes:" << endl;
+        if (passes2run.empty()) out << "  " << "None" << endl;
+        for (const auto& pass : passes2run) {
+            out << "  " << pass << endl;
+        }
+    }
+
+    llvm_function_pipeline llvm_func { module_ptr };
+    llvm_func.assignLogger(*this);
+
+    llvm_func.add(*annotatedModule->annotations);
+    llvm_func.add(annotatedModule->extVars);
+    clang::FileManager files2{ FileSystemOptions() };
+    llvm.add(files2);
+
+    llvm_func.add(std::string{ "loops" });
+
+    for (StringRef pass : passes2run) {
+        llvm_func.add(pass.str());
+    }
+
+    llvm::errs() << "BEWBS" << *module_ptr << '\n';
+
+    llvm_func.run();
 
     // verify we didn't screw up the module structure
 
